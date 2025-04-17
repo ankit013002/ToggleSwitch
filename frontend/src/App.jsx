56ba869ffa867efea5d5a9db808778dd4194569e
ankit013002 <patel.3854@osu.edu>
2025-04-17 13:16:15 -0400

    Implemented a menu selection area. Want to refine it and also avoid it opening in the center and instead opening downwards.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 import ToggleSwitch from "./components/ToggleSwitch";
+import MenuSelection from "./components/MenuSelection";
 
 function App() {
+  // Toggle Switch
   const [isToggled, setIsToggled] = useState(false);
+
+  // Menu selection
+  const [options, setOptions] = useState(["a","b","a","b","a","b","a","b"])
+  const [selectedItem, setSelectedItem] = useState(options[0])
+
 
   return (
     <>
       <div className="main-container">
         <ToggleSwitch isToggled={isToggled} setIsToggled={setIsToggled} />
+        <div className="main-menu-selection-container">
+          <MenuSelection selectedItem={selectedItem} setSelectedItem={setSelectedItem} options={options}/>
+        </div>
       </div>
     </>
   );
---
+++ frontend/src/components/MenuSelection.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import "./MenuSelection.css"
+
+const MenuSelection = ({selectedItem, setSelectedItem, options}) => {
+
+  const [expand, setExpand] = useState(false)
+
+  return (
+    <div className={`menu-selection-container ${expand ? "menu-selection-container-expanded" : ""}`}>
+      <div className={`selected-item-container ${expand &&" selected-item-container-expanded"}`}>
+          <button onClick={() => {setExpand(!expand)}} className="selected-item-button">{selectedItem}</button>
+      </div>
+      {expand &&
+        <div className="menu-selection-options-container">
+          {options.map((item, index) => (
+            <div key={index} className="menu-selection-options-button-container">
+              <button  className="menu-selection-options-button" onClick={() => {setExpand(false); setSelectedItem(item)}}>{item}</button> 
+            </div>
+          ))}
+        </div>
+      }
+    </div>
+  )
+}
+
+export default MenuSelection
