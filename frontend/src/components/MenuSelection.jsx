90711abb217d52b0813239bd3cc34348760ceccb
ankit013002 <patel.3854@osu.edu>
2025-04-17 13:39:31 -0400

    Made the menu selection more visually appealing.

--- frontend/src/components/MenuSelection.jsx
+++ frontend/src/components/MenuSelection.jsx
@@ -7,14 +7,14 @@ const MenuSelection = ({selectedItem, setSelectedItem, options}) => {
 
   return (
     <div className={`menu-selection-container ${expand ? "menu-selection-container-expanded" : ""}`}>
-      <div className={`selected-item-container ${expand &&" selected-item-container-expanded"}`}>
-          <button onClick={() => {setExpand(!expand)}} className="selected-item-button">{selectedItem}</button>
+      <div className={`selected-item-container ${expand ? "selected-item-container-expanded" : ""}`}>
+          <button aria-label="Toggle menu" aria-expanded={expand} onClick={() => {setExpand(!expand)}} className="selected-item-button">{selectedItem}</button>
       </div>
       {expand &&
         <div className="menu-selection-options-container">
           {options.map((item, index) => (
             <div key={index} className="menu-selection-options-button-container">
-              <button  className="menu-selection-options-button" onClick={() => {setExpand(false); setSelectedItem(item)}}>{item}</button> 
+              <button className="menu-selection-options-button" onClick={() => {setExpand(false); setSelectedItem(item)}}>{item}</button> 
             </div>
           ))}
         </div>
